401c9d96d56603f9fbfa3cc3262edf80ede2f006
Billy-Crawford <oumaroubilly6@gmail.com>
2025-09-01 13:34:13 +0200

    gestion de modif des infos du profil cote locataire

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PropertyViewSet, ContractViewSet, PaymentViewSet, MessageViewSet, RegisterAdminView, \
-    CreateLocataireView, LocataireViewSet, MeViewSet
+    CreateLocataireView, LocataireViewSet, MeViewSet, MeFlutterViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
